45a15413c16338f6c3f033f414052488ba4d0ce3
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-12-28 03:52:25 +0700

    COMMIT : Traversy Media - Laravel Crash Course 2020 Minutes : 26.00

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/posts', function () {
     return view('posts.index');
